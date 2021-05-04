10b186e6cbf9f3d645b4f74c8e40c3b353e1adf2
vmpyr <sarswat_ur@me.iitr.ac.in>
2021-05-04 20:56:44 +0530

    added request buttons

--- scripts/res-script.js
+++ scripts/res-script.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    $(window).scroll(function(){
+		if(this.scrollY > 20){
+			$('.navbar').addClass("sticky");
+		}else{
+			$('.navbar').removeClass("sticky");
+		}
+	});
+
+    $(".acad-button").hover(function(){
+        $(".p-acad").toggleClass("turn-black");
+        $(".p-acad").text("Want to request an academic book?");
+        $(".turn-black").text("Then click here!!");
+    });
+    $(".misc-button").hover(function(){
+        $(".p-misc").toggleClass("turn-black");
+        $(".p-misc").text("Want to request a miscellaneous book?");
+        $(".turn-black").text("Then click here!!");
+    });
 });
